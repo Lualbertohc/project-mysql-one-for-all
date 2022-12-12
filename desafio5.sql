63c3dca5adad6c9547c0b4134d4e84f21008491c
Luis Alberto Henriques Cardoso <lualbertohc@pop-os.localdomain>
2022-12-12 10:42:07 -0300

    quinto requisito

---
+++ desafio5.sql
@@ -0,0 +1,9 @@
+SELECT 
+m.musica AS cancao,
+COUNT(h.musica_id) AS reproducoes
+FROM SpotifyClone.musicas AS m
+INNER JOIN SpotifyClone.historico_de_reproducoes AS h
+ON m.musica_id = h.musica_id
+GROUP BY cancao 
+ORDER BY reproducoes 
+DESC, cancao ASC LIMIT 2;
